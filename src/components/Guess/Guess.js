6e270f069907a1c5b69af619d7577118e988c4f6
Eduardo Pereira <egpereira05@gmail.com>
2024-07-20 16:15:59 -0300

    Add exercise 4 solution

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
 import { range } from '../../utils'
+import { checkGuess } from '../../game-helpers'
 
-function Guess({ value = '' }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell'
+
+  return <span className={className}>{letter}</span>
+}
+
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer)
+
   return (
     <p className='guess'>
       {range(5).map((i) => (
-        <span className='cell' key={i}>
-          {value[i]}
-        </span>
+        <Cell key={i} letter={result?.[i].letter} status={result?.[i].status} />
       ))}
     </p>
   )
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -5,11 +5,11 @@ import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
 import Guess from '../Guess'
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className='guess-results'>
       {range(NUM_OF_GUESSES_ALLOWED).map((i) => (
-        <Guess key={i} value={guesses[i]} />
+        <Guess key={i} value={guesses[i]} answer={answer} />
       ))}
     </div>
   )
